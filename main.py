2f581e535167395c5c60243c46ddbcda37eeb467
OlgaVino25 <olgavinokyrova25@yandex.ru>
2026-04-17 13:08:25 +0300

    Add CLI options

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import configargparse
+
 from chat import connect_to_chat, read_messages
 from logger import ChatLogger
 
@@ -6,12 +8,47 @@ HOST = "minechat.dvmn.org"
 PORT = 5000
 
 
+def parse_args():
+    parser = configargparse.ArgParser(
+        default_config_files=["settings.ini"],
+        description="Клиент для чата Minecraft.",
+    )
+
+    parser.add_argument(
+        "-c", "--config", is_config_file=True, help="Путь к файлу конфигурации"
+    )
+    parser.add_argument(
+        "--host",
+        help="Хост чат-сервера",
+        env_var="MINECHAT_HOST",
+        default="minechat.dvmn.org",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        help="Порт чат-сервера",
+        env_var="MINECHAT_PORT",
+        default=5000,
+    )
+    parser.add_argument(
+        "--history",
+        help="Путь к файлу истории",
+        env_var="MINECHAT_HISTORY",
+        default="chat_history.log",
+    )
+
+    args = parser.parse_args()
+    return args.host, args.port, args.history
+
+
 async def main():
-    logger = ChatLogger("chat_history.log")
+    host, port, history_file = parse_args()
+
+    logger = ChatLogger(history_file)
     await logger.log_message("Установлено соединение")
 
     try:
-        reader, writer = await connect_to_chat(HOST, PORT)
+        reader, writer = await connect_to_chat(host, port)
         await read_messages(reader, logger.log_message)
     except (ConnectionError, OSError, asyncio.IncompleteReadError) as e:
         print(f"Ошибка: {e}. Попробуйте перезапустить.")
